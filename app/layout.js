7a340e89dc26e9ddadd93f3c0470887a3aa9c73d
rkhines2022 <rkhines@cox.net>
2023-06-30 11:08:50 -0700

    first commit

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import Providers from './Providers'
+import Header from './components/Header'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'IMDB Clone',
+  description: 'This is the IMBd clone website',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>
+          {/* Header */}
+          <Header />
+
+          {/* Navbar */}
+          {/* SearchBox */}
+          {children}
+        </Providers>
+
+      </body>
+    </html>
+  )
+}
